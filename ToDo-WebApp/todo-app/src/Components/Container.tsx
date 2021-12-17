139202c0433761511d1781ea969c9f60bec384ad
sooraj888 <soorajsagar888@gmail.com>
2021-12-17 16:19:49 +0530

    Todo app version 3

--- ToDo-WebApp/todo-app/src/Components/Container.tsx
+++ ToDo-WebApp/todo-app/src/Components/Container.tsx
@@ -4,23 +4,23 @@ import s from "../Components/Container.module.css";
 import TodoList from "./TodoList";
 
 function Container() {
-  let textod;
   let jsonObj;
-  let objJson;
-  const handleAddTodoBtn = (e: any) => {
-    localStorage.setItem("rememberMe", "jsonObjad");
-    textod =
-      '{ "employees" : [' +
-      '{ "firstName":"John" , "lastName":"Doe" },' +
-      '{ "firstName":"Anna" , "lastName":"Smith" },' +
-      '{ "firstName":"Peter" , "lastName":"Jones" },' +
-      '{"dsa":"ads"} ]}';
-    jsonObj = JSON.parse(textod);
+  let textod =
+    '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" },' +
+    '{"dsa":"ads"} ]}';
 
-   
-    console.log("json obj", jsonObj.employees);
-    objJson = jsonObj.employees;
-    localStorage.setItem("names", JSON.stringify(objJson));
+  let storageValue = localStorage.getItem("names");
+
+  const handleAddTodoBtn = (e: any) => {
+    jsonObj = JSON.parse(textod).employees;
+
+    localStorage.setItem("names", JSON.stringify(jsonObj));
+    console.log("json obj sdaadasd", jsonObj);
+
+    // let jsO = JSON.parse("storageValue");
   };
 
   return (
@@ -33,8 +33,10 @@ function Container() {
         <h1 className={s.titleOfComponent}>Todo List</h1>
       </div>
       {/* Todo-list  */}
-      <TodoList jsonObj={objJson}></TodoList>
-      <div>isdfs</div>
+      <TodoList jsonObj={jsonObj} prop="this is from container "></TodoList>
+
+      <div className={s.containerBody}>ggfdg </div>
+      <div className={s.containerBody}>{storageValue} </div>
     </div>
   );
 }
--- ToDo-WebApp/todo-app/src/Components/TodoList.tsx
+++ ToDo-WebApp/todo-app/src/Components/TodoList.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import styles from "../Components/TodoList.module.css"
-const TodoList = ({sad}:any) => {
+import styles from "../Components/TodoList.module.css";
+const TodoList = ({ jsonObj, prop }: any) => {
   const arr = ["sa", "asd", "bb", "cc", "dd"];
+  console.log("this message is from TodoList", jsonObj);
 
   return (
     <div>
-    
       {arr.map((item: any) => (
         <div className={styles.iems}>{item}</div>
-      
       ))}
+      {/* {jsonObj.map((items:any)=>(<div>sad</div>))}
+     <div className={styles.iems}>{}</div> */}
+      <div className={styles.iems}>{"asda "}this sada</div>
+      <div className={styles.iems}>{prop}</div>
     </div>
   );
 };
