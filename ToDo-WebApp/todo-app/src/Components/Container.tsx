c52f82acd7f13e9be994e0374c3ab7bacc7043b5
sooraj888 <soorajsagar888@gmail.com>
2021-12-18 23:11:58 +0530

    version 7

--- ToDo-WebApp/todo-app/src/Components/Container.tsx
+++ ToDo-WebApp/todo-app/src/Components/Container.tsx
@@ -2,23 +2,45 @@ import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import s from "../Components/Container.module.css";
 import TodoList from "./TodoList";
+import { json } from "node:stream/consumers";
 
 function Container() {
-  let textonj =
-    ' [{"firstName":"sssssssss","lastName":""},{"firstName":"John","lastName":"Doe"},{"firstName":"Anna","lastName":"Smith"},{"firstName":"Peter","lastName":"Jones"}]';
+  let data: any;
+  if (localStorage.getItem("data") == null) {
+    localStorage.setItem("data", JSON.parse("[]"));
+  }
 
-  console.log("111 simple string", textonj);
+  const handleAddTodoBtn = () => {
+    console.log("hi");
+    data = localStorage.getItem("data");
+    console.log("data fetch", data);
+    console.log("17 fson parsed data", data);
 
-  let jsonObj = JSON.parse(textonj);
+    data = JSON.parse('[{"sad":"asaaa1"},{"sad":"sa2"}]');
 
-  localStorage.setItem("names", JSON.stringify(textonj));
-  
-  let listInLocalStorage = localStorage.getItem("names");
+    
 
-  const handleAddTodoBtn = (e: any) => {
-    // console.log("105 json obj ", jsonObj);
+
+
+    // for (let i = 0; i < data.length; i++) {
+    //   console.log("data is", data[i]);
+    // }
+
+    //' + JSON.stringify(data) + '
+    // data[data.length] = JSON.parse('{"sad":"sda3"}');
+    // data[data.length] = JSON.parse('{"sad":"sdadsfs4"}');
+    // data[data.length] = JSON.parse('{"sad":"sda5"}');
+
+    localStorage.setItem("data", JSON.stringify(data));
+
+    console.log("data 111", localStorage.getItem("data"));
+
+    console.log("109-11:54am", data);
+
+    // for (let i = 0; i < data.length; i++) {
+    //   console.log("data", data[i]);
+    // }
   };
-
   return (
     <div className={s.containerBody}>
       {/* headding */}
@@ -29,8 +51,8 @@ function Container() {
         <h1 className={s.titleOfComponent}>Todo List</h1>
         <div></div>
       </div>
-      {/* Todo-list  */}
-      <TodoList listInLocalStorage={textonj}></TodoList>
+
+      <TodoList></TodoList>
     </div>
   );
 }
--- ToDo-WebApp/todo-app/src/Components/TodoList.tsx
+++ ToDo-WebApp/todo-app/src/Components/TodoList.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import styles from "../Components/TodoList.module.css";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-const TodoList = ({ listInLocalStorage }: any) => {
-  let data = JSON.parse(listInLocalStorage);
+const TodoList = () => {
+  let data;
+  
+  if (data == null) {
+    data = JSON.parse('[{"sad":"asaaa"},{"sad":"sa"}]');
+  }
 
-  console.log("109.1 strig data", listInLocalStorage);
+  data[data.length] = JSON.parse('{"sad":"sda"}');
+  data[data.length + 1] = JSON.parse('{"sad":"sda"}');
   console.log("109-11:54am", data);
+
+  for (let i = 0; i < data.length; i++) {
+    console.log("data", data[i]);
+  }
 
   return (
     <div>
@@ -15,10 +24,14 @@ const TodoList = ({ listInLocalStorage }: any) => {
           <>
             <div className={styles.iems}>
               <span>
-                {/* <input type="checkbox" id="switch" className={styles.checkbox}/>
-                <label htmlFor="switch" className={styles.toggle} /> */}
+                {/* <input
+                  type="checkbox"
+                  id="switch"
+                  className={styles.checkbox}
+                /> */}
+                <label htmlFor="switch" className={styles.toggle} />
               </span>
-              {items?.firstName}
+              {items?.sad}
               <span>
                 <button className={styles.deleteButton}>
                   <DeleteIcon></DeleteIcon>
