50d1955da63aa3c316a793a6944e3a6073bcc5e9
sooraj888 <soorajsagar888@gmail.com>
2021-12-25 16:36:31 +0530

    upto exit button

--- CardGame/card-game/src/App.tsx
+++ CardGame/card-game/src/App.tsx
@@ -3,15 +3,17 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [isGameSceen, setIsGameSceen] = useState(false);
+  const [isGameEnd, setIsGameEnd] = useState(false);
   const [playerName, setPlayerName] = useState("");
   const [enteredNumber, setEnterdNumber] = useState<number>(NaN);
+
+  
 
   const handleOnInputFormSubmit = (e: any) => {
     e.preventDefault();
     console.log("submited");
-
-    setPlayerName("");
-    setEnterdNumber(NaN);
+    setIsGameSceen(true);
   };
 
   const handleOnNumberChange = (e: any) => {
@@ -20,33 +22,55 @@ function App() {
   const handleOnNameChange = (e: any) => {
     setPlayerName(e.target.value);
   };
+  const handleExitOnClick = () => {
+    setIsGameSceen(false);
+    setPlayerName("");
+    setEnterdNumber(NaN);
+  };
 
   return (
     <div className="App">
-      <h1>Card game</h1>
-      <form onSubmit={handleOnInputFormSubmit}>
-        <input
-          value={playerName}
-          type="text"
-          autoComplete="off"
-          onChange={handleOnNameChange}
-          placeholder="Enter your name"
-          required
-        ></input>
-        <br></br>
-        <input
-          value={enteredNumber || ""}
-          type="number"
-          onChange={handleOnNumberChange}
-          required
-          autoComplete="off"
-          placeholder="Enter a number between 1 to 9"
-          min="1"
-          max="9"
-        ></input>
-        <br />
-        <button type="submit">Start</button>
-      </form>
+      {!isGameSceen ? (
+        <div>
+          <h1>Card game</h1>
+          <form onSubmit={handleOnInputFormSubmit}>
+            <input
+              value={playerName}
+              type="text"
+              autoComplete="on"
+              onChange={handleOnNameChange}
+              placeholder="Enter your name"
+              required
+            ></input>
+            <br></br>
+            <input
+              value={enteredNumber || ""}
+              type="number"
+              onChange={handleOnNumberChange}
+              required
+              autoComplete="off"
+              placeholder="Enter a number between 1 to 9"
+              min="1"
+              max="9"
+            ></input>
+            <br />
+            <button type="submit">Start</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          {playerName}
+          {enteredNumber}
+          {!isGameEnd ? (
+            <></>
+          ) : (
+            <>
+              <button>Retry</button>
+              <button onClick={handleExitOnClick}>Exit</button>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
