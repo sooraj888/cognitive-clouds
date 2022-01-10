57062370618ee81a51863dbee8fc5ffe68e034af
sooraj888 <soorajsagar888@gmail.com>
2022-01-10 10:32:53 +0530

    upto showing data of post 0 with hardcoded

--- app-3/src/App.tsx
+++ app-3/src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [countPost, setCountPost] = useState<number>(NaN);
   const [postData, setPostData] = useState<any>([]);
+  const [selectedPost, setSelectedPost] = useState<number>(0);
   let rawCount: number = -1;
   let rawPostData: any = [];
   let myInterval: any;
@@ -17,25 +18,37 @@ function App() {
         rawPostData = [...rawPostData, data?.hits];
         setPostData(rawPostData);
         setCountPost(rawCount + 1);
-        if (rawCount >= 1) {
+        if (rawCount >= 0) {
           clearInterval(myInterval);
         }
       });
   };
 
   useEffect(() => {
-    // handleUpdateData();
-    // myInterval = setInterval(handleUpdateData, 10000);
+    handleUpdateData();
+    myInterval = setInterval(handleUpdateData, 10000);
   }, []);
   useEffect(() => {
     console.log("countPost", countPost);
   }, [countPost]);
-  useEffect(() => {
-    console.log(postData);
-  }, [postData]);
+
   return (
     <div className="App">
       <h1>List</h1>
+      {console.log(postData)}
+      {postData && postData?.[selectedPost]
+        ? postData?.[selectedPost].map((item: any) => {
+            console.log("item inside post data", item);
+            return (
+              <div key={item?.objectID}>
+                <span className="listItems">{item?.title}</span>
+                <span className="listItems">{item?.url}</span>
+                <span className="listItems">{item.author} </span>
+                <span className="listItems">{item?.created_at}</span>
+              </div>
+            );
+          })
+        : console.log("data not exits")}
     </div>
   );
 }
